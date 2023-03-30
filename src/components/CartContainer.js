f1095415a0dbf30e26562f9a7b2d87240959c5b0
Laura Veee <laura@vingada.com>
2023-03-30 13:14:35 -0400

    learned actions and dispatch and loaded up a bunch of reducers in the cartSlice and implemented them in buttons and useeffects.

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -1,7 +1,9 @@
 import CartItem from "./CartItem";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { clearCart } from "../features/cart/cartSlice";
 
 const CartContainer = () => {
+    const dispatch = useDispatch();
     const { cartItems, total, amount } = useSelector((state) => state.cart);
 
     if (amount < 1) {
@@ -31,7 +33,12 @@ const CartContainer = () => {
                             total: <span>${total}</span>
                         </h4>
                     </div>
-                    <button className="btn clear-btn">clear cart</button>
+                    <button 
+                    className="btn clear-btn"
+                    onClick={() => {
+                        dispatch(clearCart());
+                    }}
+                    >clear cart</button>
                 </footer>
             </header>
         </section>
