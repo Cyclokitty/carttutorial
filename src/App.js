112f464c11b66dba91fa4194184b6219c26e7341
Laura Veee <laura@vingada.com>
2023-03-31 16:51:45 -0400

    changed the data shown in app from a file to fetching from api.

--- src/App.js
+++ src/App.js
@@ -2,16 +2,27 @@ import { useEffect } from 'react';
 import Navbar from './components/Navbar';
 import CartContainer from './components/CartContainer';
 import { useSelector, useDispatch } from 'react-redux';
-import { calculateTotals } from './features/cart/cartSlice';
+import { calculateTotals, getCartItems } from './features/cart/cartSlice';
 import './App.css';
 
 function App() {
-  const { cartItems } = useSelector((store) => store.cart);
+  const { cartItems, isLoading } = useSelector((store) => store.cart);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(calculateTotals());
   }, [cartItems]);
+
+  useEffect(() => {
+    dispatch(getCartItems());
+  }, []);
+
+  if (isLoading) {
+    return(
+    <div className='loading'>
+      <h1>...loading...</h1>
+    </div>)
+  }
 
   return (
     <main>
